c5d7a2cb3a9788d5a2697c5a7a1b94ecfb4de88d
Wellington Moreno <jwellington.moreno@gmail.com>
2017-03-04 11:06:10 -0800

    Adds DenvyCom main class

---
+++ src/main/java/tech/sirwellington/selenium/DenvyCom.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+ 
+package tech.sirwellington.selenium;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author SirWellington
+ */
+public final class DenvyCom 
+{
+    private final static Logger LOG = LoggerFactory.getLogger(DenvyCom.class);
+
+    public static void main(String[] args)
+    {
+        
+    }
+}
