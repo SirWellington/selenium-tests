84a622523668167bc666de05d69625f240a5568d
Wellington Moreno <jwellington.moreno@gmail.com>
2017-03-07 12:41:21 -0800

    Adds Kotlin support

---
+++ src/main/java/tech/sirwellington/selenium/CAEBTWebsite.kt
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tech.sirwellington.selenium
+
+/*
+
+  @author sirwellington
+  Created on Mar 7, 2017
+*/
+
+class CAEBTWebsite 
+{
+    
+    public fun main(args: Array<String>)
+    {
+        
+        val message = """
+        | Hi this is a multi line string
+        | that can extend to multiple lines.
+        """        
+        
+        println(message)
+    }
+}
