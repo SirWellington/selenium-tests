0587fe6167d0a5b9458b47dcfa7bd5b7c5b8f7f8
Wellington Moreno <jwellington.moreno@gmail.com>
2017-03-07 16:45:38 -0800

    Adds code that loads transactions

--- src/main/java/tech/sirwellington/selenium/CAEBTWebsite.kt
+++ src/main/java/tech/sirwellington/selenium/CAEBTWebsite.kt
@@ -11,7 +11,11 @@ import org.openqa.selenium.NoSuchElementException
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
 import org.openqa.selenium.htmlunit.HtmlUnitDriver
+import org.openqa.selenium.lift.find.TableCellFinder
+import org.openqa.selenium.support.ui.Select
 import org.slf4j.LoggerFactory
+import tech.sirwellington.alchemy.arguments.FailedAssertionException
+import java.util.*
 
 /*
 
@@ -40,7 +44,9 @@ fun main(args: Array<String>)
     val foodBalance = getFoodBalance(web)
     val cashBalance = getCashBalance(web)
 
-    LOG.info("Food Balance [${foodBalance}], Cash Balance [${cashBalance}]")
+    LOG.info("Food Balance [$foodBalance], Cash Balance [$cashBalance]")
+
+    printTransactions(web)
 }
 
 private fun login(web: WebDriver)
@@ -56,32 +62,8 @@ private fun login(web: WebDriver)
 
     val submitField = web.tryToFindField(By.cssSelector("input.submit_container")) ?: return
     submitField.click()
-
 }
 
-private fun WebDriver.tryToFindField(by: By): WebElement?
-{
-    try
-    {
-        return this.findElement(by)
-    }
-    catch(ex: NoSuchElementException)
-    {
-        LOG.warn("Failed to load Element: ${by}. ${ex}")
-        return null
-    }
-}
-
-private fun WebDriver.printStatus()
-{
-    val status =
-            """
-            The current title is '${this.title}'
-            and the current url is '${this.currentUrl}'
-            """
-
-    LOG.info(status)
-}
 
 private fun getFoodBalance(web: WebDriver) : Double?
 {
@@ -99,4 +81,170 @@ private fun getCashBalance(web: WebDriver) : Double?
     val element = web.tryToFindField(By.xpath(xPath)) ?: return null
 
     return element.text.removePrefix("$").toDoubleOrNull()
+}
+
+
+private data class Transaction(val date: String,
+                               val retailer: String,
+                               val address: String,
+                               val type: String,
+                               val transactionType: String,
+                               val amount: Double)
+
+private fun printTransactions(web: WebDriver)
+{
+
+    val transactionsURL = "https://www.ebt.ca.gov/caebtclient/main.recip?transaction=true"
+    web.get(transactionsURL)
+    web.printStatus()
+
+    val transactions = getTransactions(web = web, month = 2, year = 2017)
+    web.printStatus()
+
+    transactions.forEach { LOG.info("$it") }
+}
+
+private fun getTransactions(web: WebDriver, month: Int, year: Int) : Array<Transaction>
+{
+    selectYear(web, year)
+    selectMonth(web, month)
+
+    fun clickSearch()
+    {
+        val path = """//*[@id="middle_column"]/div/form/table[1]/tbody/tr[2]/td[4]/p/input"""
+        val element = web.tryToFindField(By.xpath(path)) ?: return
+        element.click()
+    }
+
+    clickSearch()
+
+    web.printStatus()
+
+    return loadTransactions(web)
+}
+
+private fun loadTransactions(web: WebDriver): Array<Transaction>
+{
+    var transactions = arrayOf<Transaction>()
+
+    val source = web.pageSource
+    val tablePath = """//*[@id="middle_column"]/div/table[2]/tbody[2]"""
+    val tableSelector = "#middle_column > div > table:nth-child(7) > tbody:nth-child(2)"
+    val table = web.tryToFindField(By.cssSelector(tableSelector)) ?: return transactions
+
+    //Will Wagner
+    return table.tryToFindElements(By.cssSelector("tr"))
+            .map(::rowToTransaction)
+            .filterNotNull()
+            .toTypedArray()
+
+}
+
+private fun rowToTransaction(element: WebElement) : Transaction?
+{
+
+    val cells = element.tryToFindElements(By.cssSelector("td"))
+
+    if (cells.isEmpty()) return null
+
+    val date = cells[0].text ?: ""
+    val retailer = cells[1].text ?: ""
+    val location = cells[2].text ?: ""
+    val type = cells[4].text ?: ""
+    val transactionType = cells[5].text ?: ""
+    val amount = cells.last().text.removePrefix("-$").toDoubleOrNull() ?: 0.0
+
+    return Transaction(date, retailer, location, type, transactionType, amount)
+}
+
+private fun selectYear(web: WebDriver, year: Int)
+{
+    val elementId = "selectyear"
+    val element = web.findElement(By.id(elementId))
+    element.click()
+    val select = Select(element)
+    select.selectByVisibleText("$year")
+}
+
+private fun selectMonth(web: WebDriver, month: Int)
+{
+    val elementId = "selectmonth"
+    val element = web.findElement(By.id(elementId))
+    element.click()
+
+    val select = Select(element)
+    select.selectByIndex(month - 1)
+}
+
+private val Int.monthToText: String
+    get() {
+        return when(this)
+        {
+            1 -> "January"
+            2 -> "February"
+            3 -> "March"
+            4 -> "April"
+            5 -> "May"
+            6 -> "June"
+            7 -> "July"
+            8 -> "August"
+            9 -> "September"
+            10 -> "October"
+            11 -> "November"
+            12 -> "December"
+            else -> ""
+        }
+    }
+
+//MARK: Extensions
+
+private fun WebDriver.tryToFindField(by: By): WebElement?
+{
+    try
+    {
+        return this.findElement(by)
+    }
+    catch(ex: NoSuchElementException)
+    {
+        LOG.warn("Failed to load Element: $by. $ex")
+        return null
+    }
+}
+
+private fun WebDriver.printStatus()
+{
+    val status =
+            """
+            The current title is '${this.title}'
+            and the current url is '${this.currentUrl}'
+            """
+
+    LOG.info(status)
+}
+
+private fun WebElement.tryToFindElement(by: By) : WebElement?
+{
+    try
+    {
+        return this.findElement(by)
+    }
+    catch(ex: NoSuchElementException)
+    {
+        LOG.warn("Failed to load Element: $by. $ex")
+        return null
+    }
+}
+
+private fun WebElement.tryToFindElements(by: By) : Array<WebElement>
+{
+
+    try
+    {
+        return this.findElements(by)?.filterNotNull()?.toTypedArray() ?: emptyArray()
+    }
+    catch(ex: NoSuchElementException)
+    {
+        return emptyArray()
+    }
+
 }
